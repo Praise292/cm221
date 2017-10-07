c29eb1afd006af641bfb07761ad7919fe9a3d572
Praise292 <32544765+Praise292@users.noreply.github.com>
2017-10-07 12:30:13 +0700

    อะไรก็ได้

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+
+const homeController = require('./controllers/home.controller')
+const newsController = require('./controllers/news.controller')
+
+app.get('/', homeController.index)
+app.get('/news', newsController.index)
+
+app.use('/assets',express.static(__dirname + '/static/assets',{
+	maxAge:8640000
+}))
+	
+app.listen(8000, function(err){
+	console.log('Server is running on port 8000')
+})
